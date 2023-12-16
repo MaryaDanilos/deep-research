d4fa2633f33ab1274941d12012e2b248c15f0f4f
Mariya Danilos <154020240+MaryaDanilos@users.noreply.github.com>
2023-12-16 21:53:40 +0200

    Create sequenser.js

---
+++ sequenser.js
@@ -0,0 +1,12 @@
+{
+  "main": "index.js"}
+
+"scripts": {
+    "start": "SLACK_WEBHOOK_URL=https://discord.com/api/webhooks/1007652077356470294/d1OZX5cCXkQluYTa3OeCkoc3XxpFK2xWov5LmOHMjz3tztm2dvWwN05utktV7dohGTnS/slack npx functions-framework --target=discordAlerts --signature-type=http"
+  }
+    "event_info": {
+      "log_message": "string",
+      "request_method": "string",
+      "request_url": "string",
+      "referrer": "string"
+}
